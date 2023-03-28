4dd2eb1c90a523496b4b65dfd9491f1b28f904b5
jainam.b <jainam.b@simformsolutions.com>
2023-03-28 18:22:27 +0530

    Performed practicals using C# OOP concepts, principles

---
+++ Practical3/Practical3/Bird.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Practical3
+{
+    internal class Bird // Parent class
+    {
+        public virtual void Voice()
+        {
+            Console.WriteLine("The Bird says: Turr Turrrrr");
+        }
+    }
+}
