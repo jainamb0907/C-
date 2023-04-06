e6b3c4e35fc36cc231f10f5ed1a43dcdf2d8003b
jainam.b <jainam.b@simformsolutions.com>
2023-04-06 18:29:33 +0530

    implemeted array of int and leanred exception_handling

---
+++ Practical5/Practical5/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Practical5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray1 = new int[5] { 99, 98, 92, 97, 95 };
+            // accessing the elements using for loop
+            Console.WriteLine("For loop :");
+            for (int i = 0; i < intArray1.Length; i++)
+                Console.WriteLine($"intArray1[{i}] = {intArray1[i]}");
+
+            try
+            {
+                Console.WriteLine(intArray1[5]); // throws IndexOutOfRange exception
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: {0}", ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("This is finally block");
+            }
+        }
+    }
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# C-
-C# learning with implementation
